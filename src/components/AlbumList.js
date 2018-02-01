c4dc18edb2214522e733a6f1c6fbdaed30e670a3
Francis Roberto <francisfrederickroberto@gmail.com>
2018-02-01 22:29:01 +1000

    Added AlbumDetails and Card components

--- src/components/AlbumList.js
+++ src/components/AlbumList.js
@@ -1,7 +1,8 @@
 // Import a library
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
 import axios from 'axios';
+import AlbumDetail from './AlbumDetail';
 // Create a register compoinent
 
 class AlbumList extends Component {
@@ -13,7 +14,9 @@ class AlbumList extends Component {
 
   renderAlbums() {
     return this.state.albums.map(album =>
-      <Text key={album.title}> { album.title } </Text>);
+      // the key={album.title} is placed to avoid error
+      <AlbumDetail key={album.title} album={album} />
+    );
   }
 
   render() {
